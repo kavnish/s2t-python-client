7b1c02631e1ea3d51365345ffe907aa7a7128cc9
Avnish Kumar <avnish.kumar.24.ak.ak@gmail.com>
2022-02-01 18:37:03 +0530

    cleanup

--- UI.py
+++ UI.py
@@ -1,14 +1,14 @@
 import pygame
 import sys
 import time
-from demo import Listener
-
+from demo import Listener, onlineSession, getStub
+import textwrap
 
 # initializing the constructor
 pygame.init()
 
 # screen resolution
-res = (500,500)
+res = (1100,1100)
 
 # opens up a window
 screen = pygame.display.set_mode(res)
@@ -33,6 +33,8 @@ height = screen.get_height()
 # defining a font
 smallfont = pygame.font.SysFont('Corbel',35)
 
+tfont = pygame.font.SysFont('Corbel',20)
+
 # rendering a text written in
 # this font
 
@@ -40,11 +42,10 @@ recording = False
 
 start = None
 
-transcript = ''
-
+session = None
 
 def quitButton():
-	position = [200,400,100,50]
+	position = [500,400,100,50]
 	if position[0] <= mouse[0] <= position[0] + position[2] and position[1] <= mouse[1] <= position[1] + position[3]:
 		pygame.draw.rect(screen,color_light, position)
 	else:
@@ -54,7 +55,7 @@ def quitButton():
 	return
 
 def recordButton():
-	position = [200,200,100,50]
+	position = [500,200,100,50]
 	if position[0] <= mouse[0] <= position[0] + position[2] and position[1] <= mouse[1] <= position[1] + position[3]:
 		pygame.draw.rect(screen,color_light, position)
 	else:
@@ -72,10 +73,10 @@ def recordButton():
 
 def checkmouseclick():
 
-	quit_position = [200,400,100,50]
-	record_position = [200,200,100,50]
+	quit_position = [500,400,100,50]
+	record_position = [500,200,100,50]
 
-	global recording, start
+	global recording, start, session
 
 	#checks if a mouse is clicked
 	if ev.type == pygame.MOUSEBUTTONDOWN:
@@ -84,29 +85,31 @@ def checkmouseclick():
 			pygame.quit()
 		if record_position[0] <= mouse[0] <= record_position[0] + record_position[2] and record_position[1] <= mouse[1] <= record_position[1] + record_position[3]:
 			recording = True
-			recordStart()
+			session = recordStart()
 	if ev.type == pygame.MOUSEBUTTONUP:
 		if record_position[0] <= mouse[0] <= record_position[0] + record_position[2] and record_position[1] <= mouse[1] <= record_position[1] + record_position[3]:
 			recording = False
 			start = time.time()
-			recordsStop()
+			recordStop()
+	return session
 
-	return
-
-def recordsStart():
+def recordStart():
 	stub = getStub()
-    session = onlineSession()
-    session.startTranscription(stub)
-	return
+	session = onlineSession()
+	session.startTranscription(stub)
+	return session
 
 def recordStop():
+	global session
 	session.stopTranscription()
 	return
 
 def printTranscript(position = [20, 250]):
-	global recording, transcript
-	if recording:
-		text = smallfont.render(transcript , True , color)
+	global recording, session
+	if recording or session is not None:
+		transcript = session.transcript
+		transcript = textwrap.fill(transcript, 50)
+		text = tfont.render(transcript , True , color_dark)
 		screen.blit(text , (position[0]+20, position[1]+10))
 	return 
 
